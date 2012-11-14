a4709f3b1e4781e9b98789f5be55a62614b6b041
Hannes Ovrén <hannes@dunst.isy.liu.se>
2012-11-14 11:22:04 +0100

    Remove missing header file

--- kinrs_visualizer.cpp
+++ kinrs_visualizer.cpp
@@ -7,7 +7,7 @@
 
 #include <boost/thread/thread.hpp>
 #include <pcl/common/common_headers.h>
-#include <pcl/features/normal_3d.h>
+//#include <pcl/features/normal_3d.h>
 #include <pcl/io/pcd_io.h>
 #include <pcl/io/vtk_lib_io.h>
 #include <pcl/visualization/pcl_visualizer.h>
